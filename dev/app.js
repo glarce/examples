e9d79cc6f7b9ec1cb0f8abe61cafadedf1a69d4c
trickypr <trickypr@users.noreply.github.com>
2019-04-04 16:03:27 +1100

    Add aframe suport

--- dev/app.js
+++ dev/app.js
@@ -1,34 +1,56 @@
 const Glarce = require("Glarce")
 const app = new Glarce()
 
+const aframeTest = require('./aframe/aframe')
+
 app.set("publicPath", "/Glarce")
 app.set("devPublicPath", "/")
 
-app.get(5, (res, req) => {
-  res.interactivity({
-    3: {
+app.get(5, (res, req) =>
+{
+  res.type('video')
+
+  res.interactivity(
+  {
+    3:
+    {
       type: 'alert',
       title: 'Pause',
       text: 'Continue watching...'
     },
-    5: {
+    5:
+    {
       type: 'question',
       title: 'Jump',
       text: 'Do you want to jump to 5 seconds',
-      buttons: [{
-          text: 'ok',
-          jumpTo: 5
-        },
-        {
-          text: 'no'
-        }
-      ]
+      buttons: [
+      {
+        text: 'ok',
+        jumpTo: 5
+      },
+      {
+        text: 'no'
+      }]
     },
-    7: {
+    7:
+    {
       type: 'end'
     }
   })
-  res.send(['videos/spook.webm', 'videos/spook.mp4'])
+  res.loadVids(['videos/spook.webm', 'videos/spook.mp4'])
+
+  res.send()
+})
+
+app.get(4, (res, req) =>
+{
+  res.type('aframe')
+
+  res.javascript(aframeTest.js)
+  res.assets(aframeTest.assets)
+  res.aframe(aframeTest.aframe)
+
+  res.send()
 })
 
 app.start()
